368822dc711be3bc2966776bcf2b7aec8b87772b
Redeamer <wehaagmans@gmail.com>
2023-07-10 00:18:11 +0200

    feat: :sparkles: Showing image on page after input selection

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
-
+import Image from 'next/image';
+import { read } from 'fs';
 const fileTypes = ['JPG', 'PNG', 'PDF'];
 
 interface IDragAndDrop {
@@ -16,21 +17,33 @@ const DragAndDrop = ({ file, setFile }: IDragAndDrop) => {
       alert('file type not allowed');
       return;
     }
+    console.log(uploadedFile);
+    // const reader = new FileReader();
+    // reader.onload = (e) => {
+    //   const imageUrl = e.target?.result as string;
+    //   console.log(imageUrl);
+    //   setSelectedImage(imageUrl);
+    // };
+    // reader.readAsDataURL(uploadedFile);
     setFile(uploadedFile);
   };
-
-  const handleFileUpload = () => {
-    if (!file) {
-      return;
-    }
-  };
+  // const [selectedImage, setSelectedImage] = useState<string>();
 
   return (
     <div>
       <h3>File Dropper</h3>
-      <input type="file" onChange={handleFileChange} />
-      <div>{file && `${file.name} - ${file.type}`}</div>
-      <button onClick={handleFileUpload}>Upload</button>
+      <input type="file" accept="image/*" onChange={handleFileChange} />
+      {/* <div>{file && `${file.name} - ${file.type}`}</div> */}
+      {/* {selectedImage && (
+        <Image
+          className="bg-red-50"
+          src={selectedImage}
+          draggable
+          alt="Uploaded"
+          height={200}
+          width={200}
+        />
+      )} */}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import DragAndDrop from '@/components/DragAndDrop';
+import Image from 'next/image';
 import { useState } from 'react';
+
+const renderImage = (file: File) => {
+  const imageUrl = URL.createObjectURL(file);
+  return (
+    <Image src={imageUrl} draggable alt="Uploaded" height={200} width={200} />
+  );
+};
 
 export default function Home() {
   const [file, setFile] = useState<File>();
@@ -8,6 +16,7 @@ export default function Home() {
     <main>
       <h1>ASCIIFY</h1>
       <DragAndDrop file={file} setFile={setFile} />
+      {file && renderImage(file)}
     </main>
   );
 }
